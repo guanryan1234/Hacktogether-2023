7df07d0773b84b701c085d9559ec544ff109e01b
guanryan1234 <guanryan@hotmail.com>
2023-03-09 17:28:20 -0800

    Pushing...

--- GraphPOC/GraphPOCBlazor/GraphPOCBlazor/Models/GraphService.cs
+++ GraphPOC/GraphPOCBlazor/GraphPOCBlazor/Models/GraphService.cs
@@ -1,9 +1,17 @@
+using Microsoft.Graph;
+using yeehee;
+using Yusuf.Corr;
+
 namespace GraphPOCBlazor.Data
 {
     public class GraphService
     {
         public GraphService() 
         { 
+           
+        var user = await graphClient.Me
+            .GetAsync();
+
         }
     }
 }
