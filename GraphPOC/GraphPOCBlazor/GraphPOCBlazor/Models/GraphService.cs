1b1f650b03eb1a2bd04f63161287d12e78d539bd
YusC3 <yusufcorr@gmail.com>
2023-03-11 20:08:37 -0800

    Simplify POC

--- GraphPOC/GraphPOCBlazor/GraphPOCBlazor/Models/GraphService.cs
+++ GraphPOC/GraphPOCBlazor/GraphPOCBlazor/Models/GraphService.cs
@@ -1,17 +1,67 @@
+using Azure.Core;
+using Azure.Identity;
 using Microsoft.Graph;
-using yeehee;
-using Yusuf.Corr;
+using Microsoft.Graph.Models;
 
 namespace GraphPOCBlazor.Data
 {
     public class GraphService
     {
-        public GraphService() 
-        { 
-           
-        var user = await graphClient.Me
-            .GetAsync();
+        private GraphServiceClient graphClient;
 
+        public GraphService() 
+        {
+            graphClient = Authorization();
+
+        }
+
+        public async Task<User> GetUserData()
+        {
+            return await graphClient.Me.GetAsync();
+        }
+
+        private GraphServiceClient Authorization()
+        {
+            //var scopes = new[] { "User.Read" };
+
+            //// Multi-tenant apps can use "common",
+            //// single-tenant apps must use the tenant ID from the Azure portal
+            //var tenantId = "common";
+
+            //// Value from app registration
+            //var clientId = "YOUR_CLIENT_ID";
+
+            //// using Azure.Identity;
+            //var options = new TokenCredentialOptions
+            //{
+            //    AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            //};
+
+            //// Callback function that receives the user prompt
+            //// Prompt contains the generated device code that you must
+            //// enter during the auth process in the browser
+            //Func<DeviceCodeInfo, CancellationToken, Task> callback = (code, cancellation) => {
+            //    Console.WriteLine(code.Message);
+            //    return Task.FromResult(0);
+            //};
+
+            //// https://learn.microsoft.com/dotnet/api/azure.identity.devicecodecredential
+            //var deviceCodeCredential = new DeviceCodeCredential(
+            //    callback, tenantId, clientId, options);
+
+            //var graphClient = new GraphServiceClient(deviceCodeCredential, scopes);
+
+            string[] scopes = { "User.Read" };
+
+            InteractiveBrowserCredentialOptions interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions()
+            {
+            };
+
+            InteractiveBrowserCredential interactiveBrowserCredential = new InteractiveBrowserCredential();
+
+            this.graphClient = new GraphServiceClient(interactiveBrowserCredential, scopes); // you can pass the TokenCredential directly to the GraphServiceClient
+
+            return graphClient;
         }
     }
 }
